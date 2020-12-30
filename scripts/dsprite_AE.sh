be623327f38134a01b77a53c8d41d75a21c7207c
ABedychaj <andrzej.bedychaj@karhoo.com>
2020-12-30 15:15:48 +0100

    script for AE

--- scripts/dsprite_AE.sh
+++ scripts/dsprite_AE.sh
@@ -11,17 +11,22 @@ python3 main.py \
 --alg=AE \
 --dset_dir=$DISENTANGLEMENT_LIB_DATA  \
 --dset_name=dsprites_full \
---traverse_z=true \
 --encoder=SimpleConv64 \
 --decoder=SimpleConv64 \
---max_iter 100000 \
---max_epoch 1500 \
+--max_iter 200000 \
+--max_epoch 1000 \
 --z_dim=8 \
---batch_size=32 \
+--batch_size=64 \
 --use_wandb=false \
---evaluation_metric mig factor_vae_metric unsupervised \
-
-
+--number_of_gausses=64 \
+--wica_loss=true \
+--lambda_wica=10000 \
+--recon_lambda=1 \
+--print_iter=1000 \
+--evaluate_iter=10 \
+--evaluation_metric mig factor_vae_metric \
+--lr_scheduler=StepLR \
+--lr_scheduler_args step_size=1000 gamma=0.9 \
 
 
 
